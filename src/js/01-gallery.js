83361cb70b23df84dca4f1e23927c225d7e365de
Anastasia <donchenko.anastasia@gmail.com>
2022-01-09 22:01:16 +0200

    DZ8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+let imgMarkup = galleryItems.map(({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}">
+    </a>`;
+}).join('');
+
+galleryList.insertAdjacentHTML('beforeend', imgMarkup);
+
+    let instance = new SimpleLightbox('.gallery a', {
+        captionsData:'alt', 
+        captionDelay: '250',
+    });
+    instance.on(() => {
+        instance.next();
+    });
+
